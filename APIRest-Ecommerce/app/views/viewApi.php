929d119ca61ede4cb12eaa7b41b89dce08da6aad
sntxDev <sntxdev@gmail.com>
2023-11-09 16:42:22 -0300

    correcion muestra de estado solicitud

--- APIRest-Ecommerce/app/views/viewApi.php
+++ APIRest-Ecommerce/app/views/viewApi.php
@@ -5,13 +5,14 @@ class ApiView
     public function response($data, $status)
     {
         header("Content-Type: application/json");
-        header("HTTP/1.1" . $status . " " . $this->_requestStatus($status));
+        header("HTTP/1.1 " . $status . " " . $this->_requestStatus($status));
         echo json_encode($data);
     }
-    public function _requestStatus($code)
+    private function _requestStatus($code)
     {
         $status = array(
             200 => "OK",
+            201 => "CREATED",
             404 => "NOT FOUND",
             500 => "Internal Server Error"
         );
